b8c393f6bd5154b039e1d320c30f04af1fc8bbdf
wangyuanhsiang <ywang91@studentmail.nait.ca>
2016-04-27 21:35:09 -0600

    Gravity Hero - Shawn 2016/04/27

---
+++ BandApp - Gravity Hero/ViewModel/ViewModel.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using System.Runtime.Serialization;
+
+namespace BandApp___Gravity_Hero.ViewModel
+{
+    [DataContract]
+    public class ViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(String propertyName)
+        {
+            PropertyChangedEventHandler pceh = PropertyChanged;
+            if(pceh != null)
+            {
+                pceh(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+
+        public void NotifyPropertyChanged(params string[] changedProperties)
+        {
+            if(changedProperties != null)
+            {
+                foreach(string property in changedProperties)
+                {
+                    OnPropertyChanged(property);
+                }
+            }
+        }
+
+        protected virtual bool SetValue<T>(ref T target, T value, params string[] changedProperties)
+        {
+            if(object.Equals(target, value))
+            {
+                return false;
+            }
+            target = value;
+            foreach (string property in changedProperties)
+            {
+                OnPropertyChanged(property);
+            }
+            return true;
+        }
+    }
+}
